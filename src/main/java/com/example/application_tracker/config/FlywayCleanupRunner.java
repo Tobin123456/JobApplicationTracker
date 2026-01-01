6c9b4274278103875ed5ac5ab4c0856dc3dec20b
Tobin Zühlke <zuhlketobin@gmail.com>
2026-01-01 17:13:10 +0100

    Added flyway database cleanup on application start for development

---
+++ src/main/java/com/example/application_tracker/config/FlywayCleanupRunner.java
@@ -0,0 +1,21 @@
+package com.example.application_tracker.config;
+
+import org.flywaydb.core.Flyway;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FlywayCleanupRunner implements CommandLineRunner {
+
+    private final Flyway flyway;
+
+    public FlywayCleanupRunner(Flyway flyway) {
+        this.flyway = flyway;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        flyway.clean();  // Force clean here
+        flyway.migrate();  // Then run migrations
+    }
+}
