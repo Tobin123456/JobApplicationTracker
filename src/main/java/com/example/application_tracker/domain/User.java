e76fb74aaad3b19174d3f48da4d4f00f15d62d57
Tobin Zühlke <zuhlketobin@gmail.com>
2026-01-04 21:40:09 +0100

    Added user concept, added user repository

---
+++ src/main/java/com/example/application_tracker/domain/User.java
@@ -0,0 +1,54 @@
+package com.example.application_tracker.domain;
+
+import jakarta.persistence.*;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq_generator")
+    @SequenceGenerator(
+            name = "user_seq_generator",
+            sequenceName = "user_seq",
+            allocationSize = 50
+    )
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private Set<String> roles;
+
+    // JPA requires a no-arg constructor!
+    protected User() {
+    }
+
+    public User(String username, String password, Set<String> roles) {
+        this.username = username;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Set<String> getRoles() {
+        return roles;
+    }
+}
